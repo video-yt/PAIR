854d03798030fe298b40836d6f83dcb6dbe5e5c3
video-yt <videoyt823@gmail.com>
2025-12-17 07:52:50 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const __path = process.cwd();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 7860;
-
+const botdl = require('./qr').botdl;
 // Import routes
 let server = require('./qr');
 let code = require('./pair');
@@ -17,7 +17,9 @@ app.use(express.static(__path + '/public')); // Serve files from the "public" di
 // Routes
 app.use('/server', server);
 app.use('/code', code);
-
+app.use('/session/bot-download',async(req,res)=>{
+    await botdl(req,res)
+})
 app.use('/pair-code', async (req, res, next) => {
     res.sendFile(__path + '/pair.html');
 });
